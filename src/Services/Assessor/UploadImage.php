b680cd188fac9c27e395463d41db215eec1fbdf8
Angel Catedral <angel.catedral.1410@gmail.com>
2021-10-08 13:00:21 -0500

    Correccion API para subir INE del asesor

--- src/Services/Assessor/UploadImage.php
+++ src/Services/Assessor/UploadImage.php
@@ -5,16 +5,21 @@ namespace App\Services\Assessor;
 use App\App\Constants;
 use App\App\Database;
 use App\Models\AssessorModel;
+use App\Models\AuthorModel;
 
 class UploadImage
 {
     private AssessorModel $assessor;
+    private AuthorModel $author;
 
     public function __construct(array $params)
     {
         $this->assessor = new AssessorModel(array(
-            'curp' => $params['curp'], 
-            'image_ine' => $params['image_ine']
+            'curp' => $params['curp'],
+            'image_ine' => $params['image_ine'],
+        ));
+        $this->author = new AuthorModel(array(
+            'author_id' => $params['author_id']
         ));
     }
 
@@ -41,12 +46,14 @@ class UploadImage
                 "CALL SP_update_asesor_image (
                     @result,
                     :curp_in,
-                    :img_ine_in
+                    :img_ine_in,
+                    :id_autores_in
                 )";
 
             $stmt = $db->prepare($sql);
             $stmt->bindParam(':curp_in', $this->assessor->curp, \PDO::PARAM_STR);
             $stmt->bindParam(':img_ine_in', $this->assessor->ineImageUrl, \PDO::PARAM_STR);
+            $stmt->bindParam(':id_autores_in', $this->author->authorId, \PDO::PARAM_INT);
 
             $stmt->execute();
 
